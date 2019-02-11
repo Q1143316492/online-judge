c425ddc940f16c7d98f622bb6a645860dfd7b169
Q1143316492 <weilin1013@qq.com>
2019-02-12 00:20:50 +0800

    基本功能都完成了。一些小细节没有处理清楚

---
+++ include/mydocker.h
@@ -0,0 +1,25 @@
+//
+// Created by cwl on 19-2-11.
+//
+
+#ifndef STUDY_MYDOCKER_H
+#define STUDY_MYDOCKER_H
+
+#include <bits/stdc++.h>
+#include <sys/wait.h>   // waitpid
+#include <sys/mount.h>  // mount
+#include <fcntl.h>      // open
+#include <unistd.h>     // execv, sethostname, chroot, fchdir
+#include <sched.h>      // clone
+
+#define STACK_SIZE (512 * 512) // 定义子进程空间大小
+
+namespace mydocker {
+
+    void changeRunDir(std::string pathname) {
+        chdir(pathname.c_str());
+    }
+
+}
+
+#endif //STUDY_MYDOCKER_H
