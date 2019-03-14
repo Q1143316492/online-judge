f6961b9f9562b1684707fb67a9b3886031ee4be0
Q1143316492 <weilin1013@qq.com>
2019-03-14 18:04:40 +0800

    测试,提交版本好像有点混乱

--- main.cpp
+++ main.cpp
@@ -1,32 +1,28 @@
+#include <bits/stdc++.h>
 #include "include/OnlineJudge.h"
+#include "include/ComplicationCpp.h"
+#include "include/mydocker.h"
+#include "include/config.h"
 
 int main(int argc, char *argv[])
 {
+
     std::cout << "========================Judging begin=========================" << std::endl;
 
     if(argc != 6) {
-        std::cout << "========================Judging begin=========================" << std::endl;
+        std::cout << "========================Judging end=========================" << std::endl;
         exit(0);
     }
 
-    /** run.sh
-     *  g++ main.cpp -std=c++11
-        mv a.out main
-        #time_limit user_problem std_in user_in std:out
-        ./main 2 ./user_pro/user_ac ./user_pro/1.in ./user_pro/user.out ./user_pro/1.out
-        ./main 2 ./user_pro/user_wa ./user_pro/1.in ./user_pro/user.out ./user_pro/1.out
-        ./main 2 ./user_pro/user_tle ./user_pro/1.in ./user_pro/user.out ./user_pro/1.out
-        ./main 2 ./user_pro/user_re ./user_pro/1.in ./user_pro/user.out ./user_pro/1.out
-        ./main 2 ./user_pro/user_tle_2 ./user_pro/1.in ./user_pro/user.out ./user_pro/1.out
-     * */
-
     int pid = fork();
 
-    std::string time = argv[1];             //代码时间限制
-    std::string path = argv[2];             //代码编译后路径
-    std::string input_file = argv[3];       //标准输入测试案例
-    std::string output_file = argv[4];      //用户输出测试案例
-    std::string answer_file = argv[5];      //标准答案
+    std::string time = argv[1];                            //代码时间限制
+    std::string path = PROJECT_PATH + argv[2];             //代码可执行程序
+    std::string input_file = PROJECT_PATH + argv[3];       //标准输入测试案例
+    std::string output_file = PROJECT_PATH + argv[4];      //用户输出测试案例
+    std::string answer_file = PROJECT_PATH + argv[5];      //标准答案
+
+    RealResult result;
 
     Problem problem(time, path, input_file, output_file, answer_file);
 
@@ -37,9 +33,9 @@ int main(int argc, char *argv[])
     if(pid == 0) {
         OnlineJudge::child_program(getpid(), problem);
     } else {
-        RealResult result;
         result.begin_time = std::chrono::system_clock::now();
         OnlineJudge::father_program(getpid(), pid, problem, result);
+        std::cout << result << std::endl;
     }
 
     std::cout << "========================Judging end=========================" << std::endl;
