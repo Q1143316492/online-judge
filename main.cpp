0bdf8e857ce8bb2c7027144b63e41096e07df7d0
Q1143316492 <weilin1013@qq.com>
2019-03-17 17:24:21 +0800

    修改一些文件名规范,添加配置文件解析功能

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-#include "include/OnlineJudge.h"
-#include "include/ComplicationCpp.h"
+#include "include/online_judge.h"
+#include "include/complication_cpp.h"
 #include "include/mydocker.h"
 #include "include/config.h"
 
@@ -14,30 +14,33 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-    int pid = fork();
+    //return 0;
 
+    //test 2 ./judging/user_ac ./judging/1.in ./judging/user.out ./judging/1.out
     std::string time = argv[1];                            //代码时间限制
     std::string path = PROJECT_PATH + argv[2];             //代码可执行程序
     std::string input_file = PROJECT_PATH + argv[3];       //标准输入测试案例
     std::string output_file = PROJECT_PATH + argv[4];      //用户输出测试案例
     std::string answer_file = PROJECT_PATH + argv[5];      //标准答案
 
+    int pid = fork();
+
     RealResult result;
 
-    Problem problem(time, path, input_file, output_file, answer_file);
+    problem problem(time, path, input_file, output_file, answer_file);
 
     if(pid < 0) {
+        // log
         exit(0);
     }
 
     if(pid == 0) {
-        OnlineJudge::child_program(getpid(), problem);
+        online_judge::child_program(getpid(), problem);
     } else {
         result.begin_time = std::chrono::system_clock::now();
-        OnlineJudge::father_program(getpid(), pid, problem, result);
+        online_judge::father_program(getpid(), pid, problem, result);
         std::cout << result << std::endl;
     }
-
     std::cout << "========================Judging end=========================" << std::endl;
 
     return 0;
