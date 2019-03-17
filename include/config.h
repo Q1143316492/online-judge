5940d41fb23b9f7a0de57cd1ab2b398397f3c5ea
Q1143316492 <weilin1013@qq.com>
2019-03-17 17:55:59 +0800

    写完了配置文件功能

---
+++ include/config.h
@@ -0,0 +1,13 @@
+//
+// Created by cwl on 19-2-11.
+//
+
+#ifndef STUDY_CONFIG_H
+#define STUDY_CONFIG_H
+
+#include <bits/stdc++.h>
+
+std::string PROJECT_PATH = "/home/cwl/CLionProjects/Study/";
+std::string DEFAULT_JUDGE_PATH = PROJECT_PATH + "judging/";
+
+#endif //STUDY_CONFIG_H
